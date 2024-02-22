c9fa4368502ddfde2f471ef976081519f95a507b
Shreya Patil <I24.Shreya.P@FinIQ-SP.com>
2024-02-22 16:43:50 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,48 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm
+  defaultOption='pet';
+  answer='';
+
+  genders=['Male','Female'];
+  defaultGender = 'Male'
+
+  user={ username:'',
+         email:'',
+         secretQuestion:'',
+         answer:'',
+         gender:''
+
+  };
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'Female'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+      gender: 'Male'
+    });
   }
+
+  Submitted=false;
   onSubmit(form: NgForm){
-    console.log(form)
+    this.Submitted=true;
+    this.user.username=this.signupForm.value.userData.username;
+    this.user.email=this.signupForm.value.userData.email;
+    this.user.secretQuestion=this.signupForm.value.secret;
+    this.user.answer=this.signupForm.value.questionAnswer;
+    this.user.gender=this.signupForm.value.gender;
   }
 }
